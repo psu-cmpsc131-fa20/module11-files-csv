d90419bd1b4bdf08c70ab951d28a378a1c4e6f73
Yanling Wang <yanling.psu@gmail.com>
2020-11-10 14:52:04 +0000

    lab11_demo

---
+++ lab11_demo.py
@@ -0,0 +1,20 @@
+from sys import argv
+import csv
+def run():
+  if len(argv) < 3:
+    print(f"Usage: python3 {argv[0]} originalfile newfile")
+    return
+  
+  with open(argv[1], newline='') as original:
+    csvOriginal = csv.DictReader(original, dialect='excel')
+    with open(argv[2], "w", newline='') as newfile:
+      csvNew = csv.DictWriter(newfile, csvOriginal.fieldnames, dialect='excel-tab')
+      csvNew.writeheader()
+      rows = []
+      for row in csvOriginal:
+        rows.append(row)
+      sortedrows = sorted(rows, key = lambda row:row['Last'], reverse=True)  
+      csvNew.writerows(sortedrows)
+
+if __name__ == "__main__":
+  run()
